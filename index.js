75b09a6f89ea3b0cb294dc1dc7c909ef45e01b34
Sharkzdev <96703826+Sharkzdev@users.noreply.github.com>
2022-12-18 21:07:57 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ circle = document.getElementById("circle");
 score = document.getElementById("score");
 timer = document.getElementById("timer");
 let score_count = 0;
+let audio = new Audio('click.wav');
 
 function clicked(){
+    audio.play();
     score_count += 1;
     score.innerHTML = "score: " + score_count;
     RandomXpos = Math.random() * (window.innerWidth-100);
@@ -16,16 +18,19 @@ function clicked(){
 }
 
 function hard(){
+    audio.play();
     circle.style.width = "15px";
     circle.style.height = "15px";
 }
 
 function medium(){
+    audio.play();
     circle.style.width = "45px";
     circle.style.height = "45px";
 }
 
 function easy(){
+    audio.play();
     circle.style.width = "60px";
     circle.style.height = "60px";
 }
